9c3c644a2a854ff98dc85971d683c268c536aa87
JaeHyun Kim <devjaedol@gmail.com>
2021-10-16 18:15:55 +0900

    메인 페이지 추가

--- _pages/about.md
+++ _pages/about.md
@@ -7,4 +7,14 @@ entries_layout: grid
 classes: wide
 author_profile: true
 ---
+![image-right](/assets/images/jaedol_100_200.jpg){: .align-right}
 
+- Project Reader & SW Engineer
+    - LG Electronic
+        - Digital Signage's Solution development
+        - LG SuperSign serise development
+- Interests and areas of work
+    - Web Services
+    - Cloud Platform (AWS)
+    - On-Premise package Solution
+    - Databases
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "MarkDwon 사용법",
+        "excerpt":"MakrDown에 사용 법에 대해서 간략히 정리하겠습니다. 확장자 *.md 입니다. 대표적 예제 Readmd.md 두줄 밑줄을 통해서 헤더 설정 ============= 두줄 밑줄을 통해서 헤더 설정 한줄 밑줄을 통해서 헤더 설정 ------- 한줄 밑줄을 통해서 헤더 설정 # 제목 샘플 입니다. H1 ## 제목 샘플 입니다. H2 ### 제목 샘플 입니다. H3 ####...","categories": ["etc"],
+        "tags": ["etc","mk","markdown"],
+        "url": "/etc/markdown/",
+        "teaser": null
+      },{
         "title": "GitHub blog 오픈",
         "excerpt":"GitHub Blog를 개설 했습니다.   Tistory에 Blog들이 Kakao로 통합되면서, 관리가 어려운 상황이 되어서, Git Blog로 만들어 보았습니다.   꿰보는 (꿰)어야 (보)배에서 아이디어를 얻었고, 개발과 관련된 학습 시행착오를 정리할 예정입니다.   ","categories": ["blog"],
         "tags": ["blog"],
