db43fd4fc820335291b2f3a654d8037f0a277265
JaeHyun Kim <devjaedol@gmail.com>
2021-10-17 17:13:14 +0900

    구글 검색 추가 https://search.google.com/search-console

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "MarkDown 사용법",
         "excerpt":"MakrDown에 사용 법에 대해서 간략히 보시죠. 확장자 *.md 입니다. 대표적 예제 Readmd.md. 사용처, 경량 기반 텍스트 마크업 언어   정보 개발 존 그루버, 에런 스워츠 발표일 2004년 3월 19일 최신 버전 1.0.1 (2004년 12월 17일) 포맷 종류 마크업 언어 두줄 밑줄을 통해서 헤더 설정 ============= 두줄 밑줄을 통해서 헤더 설정...","categories": ["etc"],
-        "tags": ["etc","mk","markdown"],
+        "tags": ["mk","markdown"],
         "url": "/etc/markdown/",
         "teaser": null
       },{
