22170f5829d6ffb78e6689f73654b90f20ed2d70
JaeHyun Kim <devjaedol@gmail.com>
2022-05-31 14:52:39 +0000

    GitHub Action CI

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -154,4 +154,10 @@ var store = [{
         "tags": ["enumerate","for"],
         "url": "/python/python_enumerate/",
         "teaser": null
+      },{
+        "title": "[알고리즘] 자료 구조의 종류",
+        "excerpt":"몇몇은 자주 사용하지만, 알고 있으면 편리한 자료구조 목차. 자세한 샘플들은 CodingTest 메뉴에서 확인 필수. 단순 구조 정수 실수 문자 문자열 선형 구조 (선형/순차) 리스트 배열 (Array) 레코드 연결 리스트 (Linked List) 단순 연결 리스트 이중 연결 리스트 원형 연결 리스트 스택 (Stack) 큐 (Queue) 데크 (Deque) 비선형 구조 트리 (Tree)...","categories": ["algorithm"],
+        "tags": ["정수","실수","문자","문자열","리스트","배열","연결리스트","스택","큐","데크","트리","그래프","순차파일","색인파일","직접파일"],
+        "url": "/algorithm/algorithm-datastructure/",
+        "teaser": null
       }]
